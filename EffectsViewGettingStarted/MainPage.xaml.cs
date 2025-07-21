395471ceaff7ed01c3620f15e53b0f9b8663bae9
Naveenkumar S <naveenkumar.sanjeevirayan@syncfusion.com>
2025-07-21 17:08:34 +0530

    Added a demo for effects view

---
+++ EffectsViewGettingStarted/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EffectsViewGettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
